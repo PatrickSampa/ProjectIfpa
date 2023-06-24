c5084bf546c45bf33c020d91c7e892c9cf223a4d
PatrickSampa <sampa.mendonca@gmail.com>
2023-06-24 14:01:42 -0300

    localstorage

--- src/HomePage-Front/js/scripts.js
+++ src/HomePage-Front/js/scripts.js
@@ -8,49 +8,49 @@
 
 function GodOfWar(){
     console.log("localstorage")
-    localStorage.setItem('GodOfWar','../HomePage-Front/images/good of war.jpeg')
+    localStorage.setItem('ImagemJogo','../HomePage-Front/images/good of war.jpeg')
     localStorage.setItem('NomeJogo',"God Of War")
 
 }
 
 function HomemAranha(){
     console.log("localstorage")
-    localStorage.setItem('GodOfWar','../HomePage-Front/images/Homem aranha.jpg')
+    localStorage.setItem('ImagemJogo','../HomePage-Front/images/Homem aranha.jpg')
     localStorage.setItem('NomeJogo',"Marvel's Spider-Man")
 }
 
 function Assassin(){
     console.log("localstorage")
-    localStorage.setItem('GodOfWar','../HomePage-Front/images/assassin screed.jpg')
+    localStorage.setItem('ImagemJogo','../HomePage-Front/images/assassin screed.jpg')
     localStorage.setItem('NomeJogo',"Assassin's Creed")
 }
 
 function TheLast(){
     console.log("localstorage")
-    localStorage.setItem('GodOfWar','../HomePage-Front/images/the last of us.jpeg')
+    localStorage.setItem('ImagemJogo','../HomePage-Front/images/the last of us.jpeg')
     localStorage.setItem('NomeJogo',"The Last Of Us")
 }
 
 function FIFA(){
     console.log("localstorage")
-    localStorage.setItem('GodOfWar','../HomePage-Front/images/fifa.jpg')
+    localStorage.setItem('ImagemJogo','../HomePage-Front/images/fifa.jpg')
     localStorage.setItem('NomeJogo',"FIFA 23")
 }
 
 function RedDead(){
     console.log("localstorage")
-    localStorage.setItem('GodOfWar','../HomePage-Front/images/red dead.jpg')
+    localStorage.setItem('ImagemJogo','../HomePage-Front/images/red dead.jpg')
     localStorage.setItem('NomeJogo',"Red Dead Redemption 2")
 }
 
 function gta(){
     console.log("localstorage")
-    localStorage.setItem('GodOfWar','../HomePage-Front/images/gta.jpg')
+    localStorage.setItem('ImagemJogo','../HomePage-Front/images/gta.jpg')
     localStorage.setItem('NomeJogo',"Grand Theft Auto V")
 }
 
 function Resdiente(){
     console.log("localstorage")
-    localStorage.setItem('GodOfWar','../HomePage-Front/images/resident evil.jpg')
+    localStorage.setItem('ImagemJogo','../HomePage-Front/images/resident evil.jpg')
     localStorage.setItem('NomeJogo',"Residente Evil 4")
 }
--- src/Item-Front/js/scripts.js
+++ src/Item-Front/js/scripts.js
@@ -11,9 +11,10 @@
 const gameImage = document.getElementById('gameImage');
 
   // Obtém o valor do localStorage
-  const godOfWarImage = localStorage.getItem('GodOfWar');
+  const godOfWarImage = localStorage.getItem('ImagemJogo');
 
   // Define o valor do atributo src da imagem
+  console.log(godOfWarImage)
   gameImage.src = godOfWarImage;
 
 
@@ -26,3 +27,117 @@ const gameImage = document.getElementById('gameImage');
 
   // Define o conteúdo do elemento
   gameTitleElement.textContent = gameTitle;
+
+//definir o valor de cada produto  
+let valor = 0;
+if(godOfWarImage=='../HomePage-Front/images/good of war.jpeg'){
+  valor = 80
+}else if(godOfWarImage=='../HomePage-Front/images/Homem aranha.jpg'){
+  valor = 25
+}else if(godOfWarImage=='../HomePage-Front/images/assassin screed.jpg'){
+  valor = 18
+}else if(godOfWarImage=='../HomePage-Front/images/the last of us.jpeg'){
+  valor = 40
+}else if(godOfWarImage=='../HomePage-Front/images/fifa.jpg'){
+  valor = 25
+}else if(godOfWarImage=='../HomePage-Front/images/red dead.jpg'){
+  valor = 120
+}else if(godOfWarImage=='../HomePage-Front/images/gta.jpg'){
+  valor = 18
+}else if(godOfWarImage=='../HomePage-Front/images/resident evil.jpg'){
+  valor = 40
+}
+
+//INPUT QUANTIDADE DO JOGO
+var inputElement = document.getElementById('inputQuantity');
+var valorInput = inputElement.value;
+var valorTotal = valorInput*valor;
+
+//Manupulacao para pegar a quantidade de itens e fazer o calculo
+
+if(godOfWarImage == '../HomePage-Front/images/good of war.jpeg'){
+  localStorage.setItem('valorTotalGodOf', valorTotal)
+  localStorage.setItem('ProdutoGodOfWar', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/Homem aranha.jpg'){
+  localStorage.setItem('valorTotalHomemAranha', valorTotal)
+  localStorage.setItem('ProdutoHomemAranha', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/assassin screed.jpg'){
+  localStorage.setItem('valorTotalAssassin', valorTotal)
+  localStorage.setItem('ProdutoAssassin', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/the last of us.jpeg'){
+  localStorage.setItem('valorTotalTheLast', valorTotal)
+  localStorage.setItem('ProdutoGodTheLast', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/fifa.jpg'){
+  localStorage.setItem('valorTotalFifa', valorTotal)
+  localStorage.setItem('ProdutoFifa', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/red dead.jpg'){
+  localStorage.setItem('valorTotalRedDead', valorTotal)
+  localStorage.setItem('ProdutoRedDead', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/gta.jpg'){
+  localStorage.setItem('valorTotalGta', valorTotal)
+  localStorage.setItem('ProdutoGta', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/resident evil.jpg'){
+  localStorage.setItem('valorTotalResident', valorTotal)
+  localStorage.setItem('ProdutoResident', 1)
+}
+
+
+// Obtém o elemento do botão
+const button = document.querySelector('.btn-outline-dark');
+
+// Adiciona um evento de clique ao botão
+function Adicionar() {
+  // Coloque aqui o código que deseja executar quando o botão for clicado
+  let valor = 0;
+if(godOfWarImage=='../HomePage-Front/images/good of war.jpeg'){
+  valor = 80
+}else if(godOfWarImage=='../HomePage-Front/images/Homem aranha.jpg'){
+  valor = 25
+}else if(godOfWarImage=='../HomePage-Front/images/assassin screed.jpg'){
+  valor = 18
+}else if(godOfWarImage=='../HomePage-Front/images/the last of us.jpeg'){
+  valor = 40
+}else if(godOfWarImage=='../HomePage-Front/images/fifa.jpg'){
+  valor = 25
+}else if(godOfWarImage=='../HomePage-Front/images/red dead.jpg'){
+  valor = 120
+}else if(godOfWarImage=='../HomePage-Front/images/gta.jpg'){
+  valor = 18
+}else if(godOfWarImage=='../HomePage-Front/images/resident evil.jpg'){
+  valor = 40
+}
+
+//INPUT QUANTIDADE DO JOGO
+var inputElement = document.getElementById('inputQuantity');
+var valorInput = inputElement.value;
+var valorTotal = valorInput*valor;
+
+//Manupulacao para pegar a quantidade de itens e fazer o calculo
+
+if(godOfWarImage == '../HomePage-Front/images/good of war.jpeg'){
+  localStorage.setItem('valorTotalGodOf', valorTotal)
+  localStorage.setItem('ProdutoGodOfWar', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/Homem aranha.jpg'){
+  localStorage.setItem('valorTotalHomemAranha', valorTotal)
+  localStorage.setItem('ProdutoHomemAranha', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/assassin screed.jpg'){
+  localStorage.setItem('valorTotalAssassin', valorTotal)
+  localStorage.setItem('ProdutoAssassin', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/the last of us.jpeg'){
+  localStorage.setItem('valorTotalTheLast', valorTotal)
+  localStorage.setItem('ProdutoGodTheLast', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/fifa.jpg'){
+  localStorage.setItem('valorTotalFifa', valorTotal)
+  localStorage.setItem('ProdutoFifa', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/red dead.jpg'){
+  localStorage.setItem('valorTotalRedDead', valorTotal)
+  localStorage.setItem('ProdutoRedDead', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/gta.jpg'){
+  localStorage.setItem('valorTotalGta', valorTotal)
+  localStorage.setItem('ProdutoGta', 1)
+}else if(godOfWarImage=='../HomePage-Front/images/resident evil.jpg'){
+  localStorage.setItem('valorTotalResident', valorTotal)
+  localStorage.setItem('ProdutoResident', 1)
+}
+window.location.href = '../HomePage-Front/Front-home.html';
+};
